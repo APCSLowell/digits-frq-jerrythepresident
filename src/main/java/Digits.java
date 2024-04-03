d2151eb2bc69329f7b38073a1223f10734817266
jerrythepresident <142940577+jerrythepresident@users.noreply.github.com>
2024-04-03 10:50:35 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -7,8 +7,12 @@ public class Digits
 
 	public Digits(int num)
 	{ 
+        digitList = new ArrayList<Integer>(); 
+		if(num==0){
+			digitList.add(new Integer(0));
+		}
 	    while(num!=0){
-		    digitList.add(0,num%10);
+		    digitList.add(0, new Integer(num%10));
 		    num/=10;
 	    }
 	}
@@ -16,7 +20,7 @@ public class Digits
 	public boolean isStrictlyIncreasing()
 	{ 
 		for(int i=0; i<digitList.size()-1; i++){
-			if(digitList.get(i)>=digitList.get(i+1)){
+			if(digitList.get(i).intValue()>=digitList.get(i+1).intValue()){
 				return false;
 			}
 		}
